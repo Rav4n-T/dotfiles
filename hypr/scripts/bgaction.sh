7062f59325394ce50d1ffc37934b7cdc84a6832e
Rav4n-T <leonsgmali@gmail.com>
2024-05-02 01:08:48 +0800

    change output screen

--- hypr/scripts/bgaction.sh
+++ hypr/scripts/bgaction.sh
@@ -8,9 +8,9 @@ THEMEIS=$(readlink -f ~/.config/waybar/style.css | cut -d '-' -f2)
 
 #show the correct wallpapper based on the theme
 if [ $THEMEIS == "dark.css" ]; then
-  swww img --resize crop -o HDMI-A-1  ~/Pictures/wallpaper/tes5.jpg
-  swww img  -o DP-2  ~/Pictures/wallpaper/tes7.jpg
+  swww img --resize crop -o DP-2  ~/Pictures/wallpaper/tes5.jpg
+  swww img  -o HDMI-A-1  ~/Pictures/wallpaper/tes7.jpg
 else
-  swww img --resize crop -o HDMI-A-1 ~/Pictures/wallpaper/test2.jpg
-  swww img -o DP-2 ~/Pictures/wallpaper/test1.jpg
+  swww img --resize crop -o DP-2 ~/Pictures/wallpaper/test2.jpg
+  swww img -o HDMI-A-1 ~/Pictures/wallpaper/test1.jpg
 fi
