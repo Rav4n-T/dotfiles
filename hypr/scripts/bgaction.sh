ab2c15b0f0a743b3c1f6d42d601603cbbc2bce80
Rav4n-T <leonsgmali@gmail.com>
2024-09-04 04:22:58 +0800

    update font,change terminal to foot,change monitor

--- hypr/scripts/bgaction.sh
+++ hypr/scripts/bgaction.sh
@@ -8,9 +8,9 @@ THEMEIS=$(readlink -f ~/.config/waybar/style.css | cut -d '-' -f2)
 
 #show the correct wallpapper based on the theme
 if [ $THEMEIS == "dark.css" ]; then
-  swww img --resize crop -o DP-2  $HOME/.config/hypr/background/011.png
-  swww img  -o HDMI-A-1  $HOME/.config/hypr/background/009.png
+  swww img --resize crop -o DP-2  $HOME/.config/hypr/background/007.png
+  # swww img  -o HDMI-A-1  $HOME/.config/hypr/background/009.png
 else
-  swww img --resize crop -o DP-2  $HOME/.config/hypr/background/011.png
-  swww img  -o HDMI-A-1  $HOME/.config/hypr/background/009.png
+  swww img --resize crop -o DP-2  $HOME/.config/hypr/background/007.png
+  # swww img  -o HDMI-A-1  $HOME/.config/hypr/background/009.png
 fi
